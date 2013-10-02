66ba50ca1f90d85ccbb78e9e36822f0ba1ab7e68
Iñaki Abete <inakiabt@gmail.com>
2013-10-01 22:43:12 -0500

    Add backup script

---
+++ init/sublime-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dir=`dirname $0`
+
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Package\ Control.sublime-settings $dir/sublime/User/
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings $dir/sublime/User/
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/getset.sublime-snippet $dir/sublime/User/
