dc3030e3f245899abc70943626ec013a4717fc20
Iñaki Abete <inakiabt@gmail.com>
2014-02-07 14:06:02 -0300

    Updated: SublimeText preferences

--- init/sublime/backup.sh
+++ init/sublime/backup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 dir=`dirname $0`
-user_config_dir='~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/'
 
-cp -f $user_config_dir/Package\ Control.sublime-settings $dir/User/
-cp -f $user_config_dir/Preferences.sublime-settings $dir/User/
-cp -f $user_config_dir/getset.sublime-snippet $dir/User/
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Package\ Control.sublime-settings $dir/User/
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings $dir/User/
+cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/getset.sublime-snippet $dir/User/
