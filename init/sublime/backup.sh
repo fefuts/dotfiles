07a2e7178ec706d8c6b93358db88a3402195718b
Federico Freire <federico@thegoneapp.com>
2015-09-24 22:14:33 -0300

    InakiAbt's customization

--- init/sublime/backup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir=`dirname $0`
-
-cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Package\ Control.sublime-settings $dir/User/
-cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/Preferences.sublime-settings $dir/User/
-cp -f ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/getset.sublime-snippet $dir/User/
